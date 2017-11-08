885404e6e00cda3598af8eb6eed57bbad8683830
MGUERRERO\Lucas Guerrero <manuel.guerrero@intive-fdv.com>
2017-11-08 17:31:04 -0300

    agregado de cross origin para rest entre front y back

---
+++ src/main/java/com/example/demo/ComportController.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+@RestController
+public class ComportController {
+		
+    @CrossOrigin(origins = "http://localhost:8000")
+    @GetMapping("/getComPort")
+	public @ResponseBody List<Comport> getComPort() {
+		List<Comport> list = new ArrayList<>();
+		SerialPort[] portNames = SerialPort.getCommPorts();
+		for (int i = 0; i < portNames.length; i++) {
+			list.add(new Comport(portNames[i].getSystemPortName()));
+			System.out.println(portNames[i].getSystemPortName());		
+		}
+        return list;
+    }
+
+    public class Comport {
+
+        private final String comPort;
+
+        public Comport() {
+            this.comPort = "";
+        }
+
+        public Comport(String comPort) {
+            this.comPort = comPort;
+        }
+
+        public String getComport() {
+            return comPort;
+        }
+    }
+
+}
