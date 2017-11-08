1c3999a6ba7499e4ba445f528375c069136b3d06
MGUERRERO\Lucas Guerrero <manuel.guerrero@intive-fdv.com>
2017-11-07 22:26:14 -0300

    agregado de controller getComPorts que devuelve list de comport

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -1,33 +1,60 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fazecast.jSerialComm.SerialPort;
 
 @Controller
 @EnableAutoConfiguration
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Puto el que lee!";
-    }
+	@RequestMapping("/")
+	@ResponseBody
+	String home1() {
+		return "Puto el que lee!";
+	}
 
- 
-	@RequestMapping(value = "/app/{comPort}")
-	public void findComPort(@PathVariable("comPort") String comPort) {
-//		SensorGraph.getCommPorts();
-		SensorGraph.findComPortImpl(comPort);	// se testea por ejemplo con http://localhost:8080/app/COM5
-//		System.out.println(comPort);
-		
-	}    
-    
+	@RequestMapping(value = "getComPorts", method = RequestMethod.GET)
+	public @ResponseBody List<ComPorts> getComPorts() {
+		List<ComPorts> list = new ArrayList<>();
+		SerialPort[] portNames = SerialPort.getCommPorts();
+		for (int i = 0; i < portNames.length; i++)
+			list.add(new ComPorts(portNames[i].getSystemPortName()));
+		return list;
+	}
+
+	public class ComPorts {
+		private String comPort;
+
+		public ComPorts(String comPort) {
+			this.comPort = comPort;
+		}
+
+		public String getComPort() {
+			return comPort;
+		}
+
+		public void setComport(String comPort) {
+			this.comPort = comPort;
+		}
+	}
+
+	@RequestMapping(value = "/app/getComPort", method = RequestMethod.GET)
+	public void getComPort() {
+		SensorGraph.getCommPorts();
+
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
